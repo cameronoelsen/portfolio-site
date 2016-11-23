17d6846cc9f3e2ec5bd4687adb95b08f348e5ecc
cameronoelsen <cameronoelsen@gmail.com>
2016-11-22 21:32:09 -0800

    CSS refinements to Expresso page in the startup weekend section

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-   $('.slick-slider').slick({
+   $('.expresso-sw-slider').slick({
        dots: true,
        infinite: false,
        speed: 300,
        slidesToShow: 4,
-       slidesToScroll: 4,
-       variableWidth: true
+       slidesToScroll: 3,
+       variableWidth: true,
+       arrows: false
    }); 
 });
