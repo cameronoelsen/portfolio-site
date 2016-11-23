a685f3ab343d89474d66f855e8c854a340bb2e0d
cameronoelsen <cameronoelsen@gmail.com>
2016-11-22 16:25:13 -0800

    Initial Commit for my portfolio site

---
+++ js/cameronsite.js
@@ -0,0 +1,30 @@
+var pos = 0;
+var el = document.getElementById("cameron");
+var m = 0;
+window.addEventListener("scroll", function () {
+    el.style.marginTop = -window.pageYOffset/4 + "px";
+    pos = window.pageYOffset;
+}, false);
+
+$('html, body').stop().animate({
+   scrollTop: element.offset().top
+}, 1000);
+
+$(document).ready( function() {
+    $('.project-item').hover( function() {
+        $(this).find('.img-title').fadeIn(300);
+    }, function() {
+        $(this).find('.img-title').fadeOut(100);
+    });
+	
+});
+
+$(function() {
+    $(window).resize(function(){
+          if(window.matchMedia('(max-width: 767px)')){
+               $('wordmark').addClass('img-responsive');
+          }else{
+               $('wordmark').removeClass('img-responsive');
+          }
+    });
+});
