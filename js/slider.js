06e8cd98a40929c232e50647e03023199dbc894d
cameronoelsen <cameronoelsen@gmail.com>
2016-11-24 13:07:54 -0800

    Added barista section to website, added descriptive content, barista slider, barista images

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
    $('.expresso-sw-slider').slick({
-       dots: true,
-       infinite: false,
-       speed: 300,
-       slidesToShow: 4,
-       slidesToScroll: 3,
-       variableWidth: true,
-       arrows: false
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        variableWidth: true,
+        arrows: false
    }); 
     $('.expresso-mobile-slider').slick({
-       dots:true,
+        dots:true,
         infinite:false,
         speed: 300,
         slidesToShow: 6,
         slidesToScroll: 2,
+        arrows: false,
+        variableWidth: true
+    });
+    $('.expresso-barista-slider').slick({
+        dots:true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         arrows: false,
         variableWidth: true
     });
