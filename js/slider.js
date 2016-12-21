6be17229be4fa3ca6634844703d0d981d2e9814e
cameronoelsen <cameronoelsen@gmail.com>
2016-12-21 15:32:52 -0800

    Added popup for images when click on, added the functionality to the Notebook Libraries wireframe images

--- js/slider.js
+++ js/slider.js
@@ -290,4 +290,22 @@ $(document).ready(function() {
             }
         ]
     });
+    
+    //Magnific Popups for Sliders
+    $('.libraries-wireframe-slider').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Cameron Oelsen</small>';
+			}
+        }
+    });
 });
