8a738c34551937eddaf10e031bcbc24a7151a289
cameronoelsen <cameronoelsen@gmail.com>
2016-12-21 16:12:45 -0800

    Added popup slider functionality to Clock'd app page

--- js/slider.js
+++ js/slider.js
@@ -356,4 +356,36 @@ $(document).ready(function() {
 			}
         }
     });
+    $('.clockd-setup-slider').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + "<small>Clock'd iOS Application</small>";
+			}
+        }
+    });
+    $('.leaderboard-slider').magnificPopup({
+        delegate: 'img',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + "<small>Clock'd iOS Application</small>";
+			}
+        }
+    });
 });
